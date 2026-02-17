2fa087f2d474558b75b123cca4874da1a6c055da
vdk0987 <vdk0987@gmail.com>
2026-02-17 13:01:33 +0530

    new item function fixed

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,12 +1,11 @@
-#include <iterator>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hashtable.h"
 
 static ht_item* ht_new_item(char* k, char* v){
     ht_item* i = malloc(sizeof(ht_item));
-    i->key = k;
-    i->value = v;
+    i->key = strdup(k);
+    i->value = strdup(v);
     return i;
 }
