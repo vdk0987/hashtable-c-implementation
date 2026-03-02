c8ae885f8c7305f48a5b91b2d653bed6f420aa6a
vdk0987 <vdk0987@gmail.com>
2026-03-02 12:57:15 +0530

    initialise insert, search, delete functions

--- src/hashtable.h
+++ src/hashtable.h
@@ -3,7 +3,7 @@
 
 typedef struct{
     char* key;
-    char* value;    
+    char* value;
 } ht_item;
 
 typedef struct{
@@ -11,3 +11,7 @@ typedef struct{
     int count;
     ht_item** items;
 } ht_hash_table;
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
