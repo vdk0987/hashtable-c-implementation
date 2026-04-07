8c051579b0d83d7db280ec4dc884869849bf12c2
vdk0987 <vdk0987@gmail.com>
2026-04-07 21:25:29 +0530

    delete function

--- src/hash_table.c
+++ src/hash_table.c
@@ -81,3 +81,23 @@ char* ht_search(ht_hash_table* ht, const char* key){
     }
     return NULL;
 }
+
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
+void ht_delete(ht_hash_table* ht, const char* key){
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while(item != NULL){
+        if (item != &HT_DELETED_ITEM){
+            if(strcmp(item->key, key) == 0){
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    ht->count --;
+}
